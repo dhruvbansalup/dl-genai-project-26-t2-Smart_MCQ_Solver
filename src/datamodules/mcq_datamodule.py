6f49c70889f5ecea15beb87e2362606812b1125b
Dhruv Bansal <dhruvbansalup@gmail.com>
2026-07-03 10:25:26 +0530

    Change the train_test_split to StratifiedGroupKFold fo handling val_score>>public score due to duplicates prompts

--- src/datamodules/mcq_datamodule.py
+++ src/datamodules/mcq_datamodule.py
@@ -1,6 +1,6 @@
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedGroupKFold
 
 from src.config import GeneralConfig
 from src.datasets.mcq_dataset import MCQDataset
@@ -18,6 +18,7 @@ class MCQDataModule(pl.LightningDataModule):
             max_length,
             batch_size=32,
             num_workers=4,
+            fold=0
     ):
         super().__init__()
 
@@ -30,11 +31,29 @@ class MCQDataModule(pl.LightningDataModule):
         self.batch_size = batch_size
         self.num_workers = num_workers
 
+        # Helper for GroupKFold
+        self.fold=fold
+
     def setup(self, stage=None):
 
         if self.df is not None:
+
             # Split df into train and val
-            self.train_df, self.val_df = train_test_split(self.df, test_size=0.1, random_state=GeneralConfig.SEED, stratify=self.df['answer'])
+
+            # # Since the dataset have duplicate prompts, the validation score is reaching 1.0 but the public score is not. This means dataleakage is there due to duplicate prompts.
+            # self.train_df, self.val_df = train_test_split(self.df, test_size=0.1, random_state=GeneralConfig.SEED, stratify=self.df['answer'])
+
+            # So now, We will use StratifiedGroupKFold to split the dataset based on unique prompts to avoid data leakage.
+            gkf=StratifiedGroupKFold(n_splits=5, shuffle=True, random_state=GeneralConfig.SEED)
+            splits = list(gkf.split(X=self.df, y=self.df['answer'], groups=self.df["prompt"]))
+            train_idx, val_idx = splits[self.fold]
+
+            self.train_df = self.df.iloc[train_idx].reset_index(drop=True)
+            self.val_df = self.df.iloc[val_idx].reset_index(drop=True)
+            print(f"Train Dataset Size: {len(self.train_df)}")
+            print(f"Validation Dataset Size: {len(self.val_df)}")
+            print(f"Unique Prompts in Train Dataset: {self.train_df['prompt'].nunique()}")
+            print(f"Unique Prompts in Validation Dataset: {self.val_df['prompt'].nunique()}")
 
             self.train_dataset = MCQDataset(self.train_df, self.tokenizer, self.max_length, return_labels=True)
             self.val_dataset = MCQDataset(self.val_df, self.tokenizer, self.max_length, return_labels=True)
