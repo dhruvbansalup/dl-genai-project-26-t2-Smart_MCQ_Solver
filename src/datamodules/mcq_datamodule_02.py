a8765c25f30a3ca03413c95fc7c9063981a0d521
Dhruv Bansal <dhruvbansalup@gmail.com>
2026-07-21 01:27:38 +0530

    add datamodule, trainer of trasformer for the data v002

---
+++ src/datamodules/mcq_datamodule_02.py
@@ -0,0 +1,83 @@
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split, StratifiedGroupKFold
+
+from src.config import GeneralConfig
+from src.datasets.mcq_dataset import MCQDataset
+
+class MCQDataModule02(pl.LightningDataModule):
+    """
+        Gives dataloaders for datasets
+        Supporing saperate validation dataset, instead of splitting the training dataset into train and validation.
+    """
+
+    def __init__(
+            self,
+            train_df,
+            val_df,
+            test_df,
+            tokenizer,
+            max_length,
+            batch_size=32,
+            num_workers=4,
+            fold=0
+    ):
+        super().__init__()
+
+        self.train_df = train_df
+        self.val_df= val_df
+        self.test_df = test_df
+
+        self.tokenizer = tokenizer
+
+        self.max_length = max_length
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+        # Helper for GroupKFold
+        self.fold=fold
+
+    def setup(self, stage=None):
+        if self.train_df is not None:
+            self.train_dataset = MCQDataset(self.train_df, self.tokenizer, self.max_length, return_labels=True)
+        if self.val_df is not None:
+            self.val_dataset = MCQDataset(self.val_df, self.tokenizer, self.max_length, return_labels=True)
+        if self.test_df is not None:
+            self.test_dataset = MCQDataset(self.test_df, self.tokenizer, self.max_length, return_labels=False)
+
+    def train_dataloader(self):
+        if self.train_df is None:
+            return None
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            #speedup
+            num_workers=self.num_workers,
+            pin_memory=True,
+            persistent_workers=self.num_workers > 0,
+        )
+
+    def val_dataloader(self):
+        if self.val_df is None:
+            return None
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=True,
+            persistent_workers=self.num_workers > 0,
+        )
+
+    def test_dataloader(self):
+        if self.test_df is None:
+            return None
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=True,
+            persistent_workers=self.num_workers > 0,
+        )
---
+++ src/training/train_transformer_data_v002.py
@@ -0,0 +1,63 @@
+import argparse
+import pandas as pd
+
+from src.config import EnvConfig, GeneralConfig
+from scripts.make_processed_data_v002 import ensure_processed_data_exists_v002
+from src.tokenizers.hf_tokenizer import HFTokenizer
+from src.datamodules.mcq_datamodule_02 import MCQDataModule02
+from src.training.trainer import train
+from src.models.transformers.bert import BERT
+from src.models.transformers.roberta import RoBERTa
+from src.models.transformers.deberta_v3_base import DeBERTa_v3_base
+
+def train_transformer(model_class, tokenizer_model_name, log=False, uploadToKaggle=False):
+    ensure_processed_data_exists_v002()
+
+    # Loading dataset
+    train_df = pd.read_parquet(f"{EnvConfig.PROCESSED_DATA_DIR}/train_v002.parquet")
+    val_df = pd.read_parquet(f"{EnvConfig.PROCESSED_DATA_DIR}/val_v002.parquet")
+
+    # Initialize Tokenizer
+    tokenizer = HFTokenizer(model_name=tokenizer_model_name)
+
+    # Initialize Data Module
+    data_module = MCQDataModule02(
+        train_df=train_df,
+        val_df=val_df,
+        test_df=None,
+        tokenizer=tokenizer,
+        max_length=GeneralConfig.MAX_LENGTH*2+2,# we have pairs, and 2 extra tokens for [QUESTION] and [OPTION],
+        batch_size=8,
+        num_workers=4,
+    )
+
+    # Initialize Model
+    model = model_class(lr=2e-5)
+
+    # Training Configuration
+    config_logs={
+        "data_version":"v002",
+    }
+
+    # Train the model
+    train(DATA_MODULE=data_module, MODEL=model, max_epochs=6, log=log, upload_kaggle=uploadToKaggle, config_logs=config_logs)
+
+if __name__ == "__main__":
+
+    MODELS={
+        # model_name: (model_class, tokenizer_model_name)
+        "BERT":(BERT, "bert-base-uncased"),
+        "RoBERTa":(RoBERTa, "roberta-base"),
+        "DeBERTa_v3_base":(DeBERTa_v3_base, "microsoft/deberta-v3-base"),
+    }
+
+    #get cli arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, choices=MODELS.keys(), required=True)
+    parser.add_argument("--log", type=bool, default=False)
+    parser.add_argument("--uploadToKaggle", type=bool, default=False)
+    args = parser.parse_args()
+
+    model_class, tokenizer_model_name=MODELS[args.model]
+
+    train_transformer(model_class=model_class, tokenizer_model_name=tokenizer_model_name, log=args.log, uploadToKaggle=args.uploadToKaggle)
